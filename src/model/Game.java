1302e1350119e735ebd9abf4064f43c34681136c
R Sánchez <r@renems.com>
2017-05-19 21:25:05 -0500

    pre modifications

--- src/model/Game.java
+++ src/model/Game.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package model;
 
 /**
@@ -11,14 +10,14 @@ package model;
  * @author rms
  */
 public class Game {
-    
+
     public static String[] characterPlayer = {};
     public static String[] characterIA = {};
-    
+
     public static String answer = "";
     public static String[] answersPlayer = new String[10];
     public static String[] answersIA = new String[10];
-    
+
     public static int turn = 0; //1 for IA, 2 for Player
 
 }
